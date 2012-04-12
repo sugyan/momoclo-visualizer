aaeb3c19c26af662cdf1a72384abceddd4324db8
sugyan <sugi1982@gmail.com>
2012-04-13 01:57:36 +0900

    scraping script

--- bin/scrape.rb
+++ bin/scrape.rb
@@ -1,13 +1,41 @@
 #!/usr/bin/env ruby
+require 'date'
+require 'logger'
 require 'nokogiri'
 require 'open-uri'
-doc = Nokogiri::HTML(open('http://ameblo.jp/sasaki-sd/entrylist-1.html'))
-doc.css('#recent_entries_list li').each do |li|
-  data = {
-    datetime: li.css('.updatetime').text.strip,
-    title:    li.css('.newentrytitle').text,
-    count:    li.css('.cotb').text.match(/(\d+)/)[1].to_i,
-    url:      li.css('a')[0][:href],
-  }
-  puts data
+require 'pg'
+
+$log = Logger.new(STDOUT)
+$conn = PG.connect(dbname: 'momoclo')
+$member = { 1 => 'momota', 2 => 'ariyasu', 3 => 'tamai', 4 => 'sasaki', 5 => 'takagi' }
+
+def scrape (id, page)
+  name = $member[id]
+  $log.info("scarpe #{name}:#{page}")
+
+  doc = Nokogiri::HTML(open("http://ameblo.jp/#{name}-sd/entrylist-#{page}.html"))
+  doc.css('#recent_entries_list li').each do |li|
+    datetime = DateTime.parse(li.css('.updatetime').text.strip + '+09:00')
+    title    = li.css('.newentrytitle')[0].text()
+    count    = li.css('.cotb').text.match(/(\d+)/)[1].to_i()
+    url      = li.css('a')[0][:href]
+
+    if $conn.exec('SELECT url FROM item WHERE url = $1', [ url ]).count == 0
+      $conn.exec(
+        'INSERT INTO item (url, member_id, title, count, datetime) VALUES ($1::varchar, $2::integer, $3::varchar, $4::int, $5::timestamp)',
+        [ url, id, title, count, datetime ]
+      )
+      $log.debug("insert - #{url}: #{count}")
+    else
+      $conn.exec(
+        'UPDATE item SET count = $1::integer WHERE url = $2::varchar',
+        [ count, url ]
+      )
+      $log.debug("update - #{url}: #{count}")
+    end
+  end
 end
+
+# TODO
+scrape(1, 1)
+$conn.close()
--- schema.sql
+++ schema.sql
@@ -19,50 +19,29 @@ SET default_with_oids = false;
 --
 
 CREATE TABLE item (
-    id integer NOT NULL,
     url character varying(255) NOT NULL,
     count integer NOT NULL,
     title character varying(255) NOT NULL,
-    datetime timestamp without time zone NOT NULL
+    member_id integer,
+    datetime timestamp with time zone NOT NULL
 );
 
 
-ALTER TABLE public.item OWNER TO "sugyan";
-
---
--- Name: item_id_seq; Type: SEQUENCE; Schema: public; Owner: sugyan
---
-
-CREATE SEQUENCE item_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.item_id_seq OWNER TO "sugyan";
-
---
--- Name: item_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: sugyan
---
-
-ALTER SEQUENCE item_id_seq OWNED BY item.id;
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: sugyan
---
-
-ALTER TABLE ONLY item ALTER COLUMN id SET DEFAULT nextval('item_id_seq'::regclass);
-
+ALTER TABLE public.item OWNER TO sugyan;
 
 --
 -- Name: item_pkey; Type: CONSTRAINT; Schema: public; Owner: sugyan; Tablespace: 
 --
 
 ALTER TABLE ONLY item
-    ADD CONSTRAINT item_pkey PRIMARY KEY (id);
+    ADD CONSTRAINT item_pkey PRIMARY KEY (url);
+
+
+--
+-- Name: member_datetime; Type: INDEX; Schema: public; Owner: sugyan; Tablespace: 
+--
+
+CREATE INDEX member_datetime ON item USING btree (member_id, datetime);
 
 
 --
