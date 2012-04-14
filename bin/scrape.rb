5733e7d31a1c97c1cf9c081db7bf229f5d52c313
sugyan <sugi1982@gmail.com>
2012-04-15 01:10:57 +0900

    modify scrape script

--- bin/scrape.rb
+++ bin/scrape.rb
@@ -21,20 +21,20 @@ def scrape (id, page)
   doc = Nokogiri::HTML(open("http://ameblo.jp/#{name}-sd/entrylist-#{page}.html"))
   list = doc.css('#recent_entries_list li')
   list.each do |li|
-    datetime = li.css('.updatetime').text.strip + '+09:00'
+    datetime = li.css('.updatetime').text.strip
     title    = li.css('.newentrytitle')[0].text()
     count    = li.css('.cotb').text.match(/(\d+)/)[1].to_i()
     url      = li.css('a')[0][:href]
 
     if $conn.exec('SELECT url FROM item WHERE url = $1', [ url ]).count == 0
       $conn.exec(
-        'INSERT INTO item (url, member_id, title, count, datetime) VALUES ($1::varchar, $2::integer, $3::varchar, $4::int, $5::timestamp)',
+        "INSERT INTO item (url, member_id, title, count, datetime) VALUES ($1::varchar, $2::integer, $3::varchar, $4::int, $5::timestamp AT TIME ZONE 'JST')",
         [ url, id, title, count, datetime ]
       )
       $log.debug("insert - #{url}: #{count}")
     else
       $conn.exec(
-        'UPDATE item SET count = $1::integer, datetime = $2::timestamp WHERE url = $3::varchar',
+        "UPDATE item SET count = $1::integer, datetime = $2::timestamp AT TIME ZONE 'JST' WHERE url = $3::varchar",
         [ count, datetime, url ]
       )
       $log.debug("update - #{url}: #{count}")
