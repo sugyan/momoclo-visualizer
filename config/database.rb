410b4ac522c70de9e22af6ac9d404a0348ea0301
sugyan <sugi1982@gmail.com>
2012-08-12 18:07:26 +0900

    padrino skeleton

---
+++ config/database.rb
@@ -0,0 +1,7 @@
+Sequel::Model.plugin(:schema)
+Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
+Sequel::Model.db = case Padrino.env
+  when :development then Sequel.connect("postgres://localhost/momoclo_visualizer_development", :loggers => [logger])
+  when :production  then Sequel.connect("postgres://localhost/momoclo_visualizer_production",  :loggers => [logger])
+  when :test        then Sequel.connect("postgres://localhost/momoclo_visualizer_test",        :loggers => [logger])
+end
