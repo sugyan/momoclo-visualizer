ef85cb2669dc161907d7e2fc1d8bffc1d3a471b8
sugyan <sugi1982@gmail.com>
2012-08-17 22:12:29 +0900

    pretty slim

--- app/app.rb
+++ app/app.rb
@@ -35,6 +35,7 @@ class Visualizer < Padrino::Application
   # disable :flash                # Disables sinatra-flash (enabled by default if Sinatra::Flash is defined)
   # layout  :my_layout            # Layout can be in views/layouts/foo.ext or views/foo.ext (default :application)
   #
+  set :slim, :pretty => true
 
   ##
   # You can configure for a specified environment like:
