e5f6cd1932975fd8159c1f0c96626ecc6640afd6
sugyan <sugi1982@gmail.com>
2012-08-26 03:02:49 +0900

    draw chart

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,19 @@
-// Put your application scripts here
+$(function () {
+    $.getJSON('/api/blog_comments', function (res) {
+        var chart = new Highcharts.StockChart({
+            chart: {
+                renderTo: 'chart'
+            },
+	    series: $.map(res, function (array) {
+                return {
+                    data: $.map(array, function (e) {
+                        return {
+                            x: Date.parse(e.created_at),
+                            y: e.count
+                        };
+                    })
+                };
+            })
+        });
+    });
+});
