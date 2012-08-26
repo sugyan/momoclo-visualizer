108cb384ad01addfb9fffbac0509f1ce0a97f721
sugyan <sugi1982@gmail.com>
2012-08-26 22:55:21 +0900

    modify chart

--- models/entry.rb
+++ models/entry.rb
@@ -5,6 +5,7 @@ class Entry < Sequel::Model
       ret = {}
       where{created_at >= Date.new(2011, 1, 1)}.order(:created_at).each do |row|
         values = row.values
+        values[:created_at] = values[:created_at].to_i
         member = values.delete(:member)
         ret[member] ||= []
         ret[member].push(values)
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,20 @@
 $(function () {
+    var member = { momota: 0, ariyasu: 1, tamai: 2, sasaki: 3, takagi: 4 };
+    var chart = new Highcharts.StockChart({
+        chart: {
+            renderTo: 'chart'
+        },
+        colors: ['#FF0000', '#00FF00', '#FFFF00', '#FF00FF', '#800080'],
+        series: [{}, {}, {}, {}, {}]
+    });
     $.getJSON('/api/blog_comments', function (res) {
-        var chart = new Highcharts.StockChart({
-            chart: {
-                renderTo: 'chart'
-            },
-	    series: $.map(res, function (array) {
+        $.each(res, function (key, value) {
+            chart.series[member[key]].setData($.map(value, function (e) {
                 return {
-                    data: $.map(array, function (e) {
-                        return {
-                            x: Date.parse(e.created_at),
-                            y: e.count
-                        };
-                    })
+                    x: new Date(e.created_at * 1000),
+                    y: e.count
                 };
-            })
+            }));
         });
     });
 });
