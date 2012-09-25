e173d8a3c4c7f16d2e26b0e31c9e30b5bc559ba5
sugyan <sugi1982@gmail.com>
2012-09-25 15:09:39 +0900

    add comment

--- models/entry.rb
+++ models/entry.rb
@@ -4,8 +4,11 @@ class Entry < Sequel::Model
     def for_highstock
       ret = {}
       where{created_at >= Date.new(2011, 1, 1)}.order(:created_at).each do |row|
+        # create hash
         values = row.values
+        # convert to epoch
         values[:created_at] = values[:created_at].to_i
+        # push to each array
         member = values.delete(:member)
         ret[member] ||= []
         ret[member].push(values)
